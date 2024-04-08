4faa43de0b6ce892a7dfdcbec82de9e7fc7ff752
기몌나 <128577934+kimyena0602@users.noreply.github.com>
2024-04-08 23:00:30 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.01 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.java
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.java
@@ -1,27 +1,20 @@
 class Solution {
-    
-    public static long solution(int n) {
-        if (n % 2 == 1) {
-            // n= 홀수
-            long sum = 0;
-            for (int i = 1; i <= n; i += 2) {
-                sum += i;
+    public int solution(int n) {
+        int sum =0;
+        if(n%2==1){
+            for(int i =1; i<=n; i++){
+                if(i%2==1){
+                    sum+=i;
+                } 
             }
-            return sum;
-        } else {
-            // n =짝수
-            long sumOfSquares = 0;
-            for (int i = 2; i <= n; i += 2) {
-                sumOfSquares += (long) i * i;
-            }
-            return sumOfSquares;
         }
-    }
-
-    public static void main(String[] args) {
-        int oddNumber = 5;
-        int evenNumber = 6;
-        System.out.println(solution(oddNumber)); // 홀수
-        System.out.println(solution(evenNumber)); // 짝수
+        else{
+            for(int i=1; i<=n; i++){
+                if(i%2==0){
+                    sum+=i*i;
+                }
+            }
+        }
+        return sum;
     }
 }
